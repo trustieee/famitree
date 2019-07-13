3d0d47699bbbf8a67b893c63d93559c111bce593
Mario Catch <mariocatch@Marios-MacBook-Pro.local>
2019-07-12 22:43:15 -0400

    Add usage of api

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 function App() {
-  return <div>foo</div>;
+  const [genders, setGenders] = useState([]);
+  const [people, setPeople] = useState([]);
+  const [surnames, setSurnames] = useState([]);
+
+  useEffect(() => {
+    if (genders.length === 0)
+      fetch("http://localhost:3001/genders")
+        .then(res => res.json())
+        .then(res => setGenders(res));
+
+    if (people.length === 0)
+      fetch("http://localhost:3001/people")
+        .then(res => res.json())
+        .then(res => setPeople(res));
+
+    if (surnames.length === 0)
+      fetch("http://localhost:3001/surnames")
+        .then(res => res.json())
+        .then(res => setSurnames(res));
+  }, [genders, people, surnames]);
+
+  return <></>;
 }
 
 export default App;
